df9ff38266d4fdb848432db043f7e0d9a66b934a
Sven Meyer <git_dev@svenmeyer.com>
2021-01-28 17:52:15 +1100

    feat: add request for getLINKbalance

--- ethereum/scripts/get-lottery-info.js
+++ ethereum/scripts/get-lottery-info.js
@@ -4,36 +4,48 @@ const RandomNumberConsumer = artifacts.require('RandomNumberConsumer')
 module.exports = async callback => {
   try {
     const lotto = await LottoBuffalo.deployed()
-    const rando = await RandomNumberConsumer.deployed()
+    const random = await RandomNumberConsumer.deployed()
 
-    const lottoInfo = async () => {
-      let _id = await lotto.id.call()
-      let _isOpen = await lotto.isOpen()
-      let _isClosed = await lotto.isClosed()
-      let _isFinished = await lotto.isFinished()
-      let _playerCount = await lotto.getPlayerCount()
-      let _players = await lotto.getPlayers()
-      let _lotteryAmount = await lotto.getLotteryAmount()
-      let _alarmAddress = await lotto.getAlarmAddress()
-      let _alarmJobId = await lotto.getAlarmJobId()
-      let _randomResult = await lotto.RANDOMRESULT.call()
 
-      console.log(`id: ${_id}`)
-      console.log(`Stage.open: ${_isOpen}`)
-      console.log(`Stage.closed: ${_isClosed}`)
-      console.log(`Stage.finished: ${_isFinished}`)
-      console.log(`Player Count: ${_playerCount}`)
-      console.log(`Players: `, _players)
-      console.log(`Lottery Amount: ${_lotteryAmount}`)
-      console.log(`Alarm Address: ${_alarmAddress}`)
-      console.log(`Alarm Job Id: ${_alarmJobId}`)
-      console.log(`Random Result: ${_randomResult}`)
-    };
+		let _id = await lotto.id.call()
+		console.log(`id: ${_id}`)
 
-    await lottoInfo();
+		let _isOpen = await lotto.isOpen()
+		console.log(`Stage.open: ${_isOpen}`)
+
+		let _isClosed = await lotto.isClosed()
+		console.log(`Stage.closed: ${_isClosed}`)
+
+		let _isFinished = await lotto.isFinished()
+		console.log(`Stage.finished: ${_isFinished}`)
+
+		let _playerCount = await lotto.getPlayerCount()
+		console.log(`Player Count: ${_playerCount}`)
+
+		let _players = await lotto.getPlayers()
+		console.log(`Players: `, _players)
+
+		let _lotteryAmount = await lotto.getLotteryAmount()
+		console.log(`Lottery Amount: ${_lotteryAmount}`)
+
+		let _alarmAddress = await lotto.getAlarmAddress()
+		console.log(`Alarm Address: ${_alarmAddress}`)
+
+		let _alarmJobId = await lotto.getAlarmJobId()
+		console.log(`Alarm Job Id: ${_alarmJobId}`)
+
+		let _randomResult = await lotto.RANDOMRESULT.call()
+		console.log(`Random Result: ${_randomResult}`)
+
+		let _lottoLINKbalance = await lotto.getLINKbalance()
+		console.log('_lottoLINKbalance: $(_lottoLINKbalance')
+
+		let _randomLINKbalance = await random.getLINKbalance()
+		console.log('_randomLINKbalance: $(_randomLINKbalance')
 
     callback()
-  } catch (err) {
+	}
+	catch (err) {
     callback(err)
   }
 }
