0689120975a1ca0fef7f220de7d5138f6a7b106e
Sven Meyer <git_dev@svenmeyer.com>
2021-01-30 16:03:12 +1100

    project cleanup

--- ethereum/demo_kovan.sh
+++ ethereum/demo_kovan.sh
@@ -1,33 +1,8 @@
-# Lottery using Chainlink VRF
-
-## Requirements
-
-- npm / yarn
-
-## Installation
-
-```bash
-npm install
-```
-
-Or
-
-```bash
-yarn install
-```
-
-## Quick Start
-
-Deploment should work on all networks as long as the right parameter are provided in `truffle-config.js`.
-Please check the scripts if they might need some adjustments as well.
-This version should work on kovan out of the box.
-
-A local install needs much more work, see some guidance further below.
-
-```bash
+clear
 rm -rf build/contracts/
-truffle compile --all
-truffle migrate --network kovan --reset
+truffle compile --all | grep -v 'SPDX-License'
+
+truffle migrate --network kovan --reset | grep -v 'SPDX-License'
 
 truffle exex --network kovan scripts/fund-contract.js
 
@@ -40,4 +15,3 @@ truffle exex --network kovan scripts/get-lottery-info.js
 truffle exex --network kovan scripts/get-lottery-info.js
 truffle exex --network kovan scripts/get-lottery-info.js
 truffle exex --network kovan scripts/get-lottery-info.js
-```
